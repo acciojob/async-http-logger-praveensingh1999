f85cfc10ed4ece387a12135ce61589944934fd32
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-16 12:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//your JS code here. If required.
-const url = "https://jsonplaceholder.typicode.com/todos/1"
-async function fetchdata(url){
-	const response = await fetch(url);
-	const data = response.json();
-	console.log(data);
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await response.json();   // convert response to JSON
+  console.log(data);
 }
-fetchdata(url);
+
+getData();
